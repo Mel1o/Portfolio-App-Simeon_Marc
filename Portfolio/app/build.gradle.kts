6fa822e19d6c1d0f9b405c35e88c186d09c8f540
Mel1o <simeon.marc.2002@gmail.com>
2024-07-29 19:44:24 +0800

    got the navigation bar to work
    working nav bar but need to fix layout later

--- Portfolio/app/build.gradle.kts
+++ Portfolio/app/build.gradle.kts
@@ -69,4 +69,5 @@ dependencies {
     implementation ("androidx.constraintlayout:constraintlayout:2.1.0")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
     kapt ("com.github.bumptech.glide:compiler:4.12.0")
+    implementation ("com.google.android.material:material:1.9.0")
 }
--- Portfolio/app/src/main/java/com/example/portfolio/MainActivity.kt
+++ Portfolio/app/src/main/java/com/example/portfolio/MainActivity.kt
@@ -2,28 +2,29 @@ package com.example.portfolio
 
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.Toast
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.bumptech.glide.Glide
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-import android.view.MenuItem;
-import android.widget.Toast;
-import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.navigation.NavigationView
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var drawerLayout: DrawerLayout
-
+    private val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -34,23 +35,28 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         Glide.with(this)
             .load(R.drawable.portfolio_animation)
             .into(imageView)
-        /*
+
+
         val toolbar: Toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
+        if (toolbar != null) {
+            Log.d(TAG, "Toolbar was found but wont be set i need help ahhh")
+            setSupportActionBar(toolbar)
+        } else {
+            Log.d(TAG, "Toolbar is Null bruh")
+        }
 
         drawerLayout = findViewById(R.id.drawer_layout)
         val navigationView: NavigationView = findViewById(R.id.nav_view)
         navigationView.setNavigationItemSelectedListener(this)
 
-        val toggle =ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav)
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav)
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, FragmentAboutMe()).commit()
+            supportFragmentManager.beginTransaction().replace(R.id.frame_layout, FragmentAboutMe()).commit()
             navigationView.setCheckedItem(R.id.nav_me)
         }
-        */
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
