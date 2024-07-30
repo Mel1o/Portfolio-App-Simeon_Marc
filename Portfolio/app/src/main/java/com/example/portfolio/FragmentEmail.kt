5dc12580fa998774f223dd6e8cf2deb8efda9cde
Mel1o <simeon.marc.2002@gmail.com>
2024-07-30 21:56:35 +0800

    seprated string and color files into their respective xml

--- Portfolio/app/src/main/java/com/example/portfolio/FragmentEmail.kt
+++ Portfolio/app/src/main/java/com/example/portfolio/FragmentEmail.kt
@@ -5,7 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.Button
+import android.widget.ScrollView
 
 class FragmentEmail : Fragment() {
 
@@ -15,6 +16,22 @@ class FragmentEmail : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_email, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val scrollView = view.findViewById<ScrollView>(R.id.scroll)
+        val button = view.findViewById<Button>(R.id.scroll_button)
+
+        button.setOnClickListener {
+            // Convert 1000dp to pixels
+            val scale = resources.displayMetrics.density
+            val y = (1000 * scale + 0.5f).toInt()
+
+            scrollView.post {
+                scrollView.scrollTo(0, 100)
+            }
+        }
     }
 
 }
